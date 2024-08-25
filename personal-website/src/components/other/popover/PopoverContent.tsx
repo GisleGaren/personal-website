573992b0b2f1e7f695ff7a3eaccbc1f4e5c9fc7f
GisleGaren <89572928+GisleGaren@users.noreply.github.com>
2024-08-25 23:26:48 +0200

    Each image bubble now pops up as it should and upon clicking the image, will fade out and in of darkness filter

--- personal-website/src/components/other/popover/PopoverContent.tsx
+++ personal-website/src/components/other/popover/PopoverContent.tsx
@@ -1,21 +1,30 @@
 import "./PopoverContent.css";
-
+import { useState } from "react";
 type PopoverContentProps = {
-    imgUrl: string;
-    headerText: string;
-    pText: string;
-}
+  imgUrl: string;
+  headerText: string;
+  pText: string;
+};
 
-export const PopoverContent = ({ imgUrl, headerText, pText}: PopoverContentProps) => {
+export const PopoverContent = ({
+  imgUrl,
+  headerText,
+  pText,
+}: PopoverContentProps) => {
+
+  const [isTextVisible, setTextVisible] = useState(true);
+
+  const handleClick = () => {
+    setTextVisible(!isTextVisible);
+  };
+
   return (
-    <div className="popover">
-      <div className="text-popover">
+    <div className="popover" onClick={handleClick}>
+      <div className={`text-popover ${isTextVisible ? '' : 'hidden'}`}>
         <h1>{headerText}</h1>
-        <p>
-          {pText}
-          </p>
+        <p>{pText}</p>
       </div>
-      <img src={imgUrl} />
+      <img src={imgUrl} alt="Picture of Gisle doing something" className={isTextVisible ? '' : 'no-filter'} />
     </div>
   );
 };
