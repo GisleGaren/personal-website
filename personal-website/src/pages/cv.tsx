a6d34ce24269acf8be1e346d7093868d1c67974d
GisleGaren <89572928+GisleGaren@users.noreply.github.com>
2025-06-04 10:49:30 +0200

    Got the styling line with ball in place

--- personal-website/src/pages/cv.tsx
+++ personal-website/src/pages/cv.tsx
@@ -5,11 +5,13 @@ const CV = () => {
         <div className="wrapper">
             <h1>Curriculum Vitae</h1>
             <p>So far I have yada yada yada yada yada </p>
+            <div className="headline-section">Work Life</div>
             <div className="cv-content">
-                <div className="headline-section">Work Life</div>
                 <div className="cv-section">
-                    <h3>June 2024 - June 2024</h3>
-                    <div>thingy</div>
+                    <h3 className="cv-date">June 2024 - June 2024</h3>
+                    <div className="cv-line">
+                        <div className="cv-dot"></div>
+                    </div>
                     <div className="cv-description">
                         <h2>Junior Dev</h2>
                         <p>Oslo, Norway</p>
